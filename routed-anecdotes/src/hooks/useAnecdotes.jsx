f987da4e99df1d5971f3dad617a6e9878e1ac878
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-10 16:31:21 +0000

    complete exercises 7.3 and 7.4

---
+++ routed-anecdotes/src/hooks/useAnecdotes.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import anecdoteService from '../services/anecdotes'
+
+const useAnecdotes = () => {
+
+    const [anecdotes, setAnecdotes] = useState([])
+
+    useEffect(() => {
+        anecdoteService.getAll().then(data => setAnecdotes(data))
+    }, [])
+
+    return {
+        anecdotes
+    }
+}
+
+export default useAnecdotes
