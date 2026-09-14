fb282602c7d5b3d6e5a1e073f07102d20081cef3
cherylfong <42821602+cherylfong@users.noreply.github.com>
2026-09-14 18:50:49 +0000

    complete exercise 7.20

--- bloglist/client/src/components/Usage.jsx
+++ bloglist/client/src/components/Usage.jsx
@@ -1,17 +1,24 @@
 const Usage = () => {
   return (
     <div>
-      <ul>
+      <h3>Site Usage</h3>
+      <ul >
         <li>
           Blogs can only be deleted by the original user who added it.{' '}
           <a href="/login">Login</a> to see delete button.
         </li>
         <li>All users including unauthenticated users can like blog items.</li>
+        <li>All users including unauthenticated users can add comments. </li>
+        <li style={{ padding: 10 }}>
+          <b>Please do not abuse adding comments.</b> This site is not
+          responsible for posted profanity as comments.
+        </li>
         <li>
           Only authenticated (logged in) users can add a new blog item to the
           list.
         </li>
-        <li>
+        <li>Only authenticated (logged in) users can delete their comment.</li>
+        <li style={{ padding: 10 }}>
           <b>There is no new user registration.</b>
         </li>
       </ul>
--- bloglist/client/src/main.jsx
+++ bloglist/client/src/main.jsx
@@ -2,20 +2,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { UserContextProvider } from './UserContext'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+})
+
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <Router>
-      <UserContextProvider>
-        <App />
-      </UserContextProvider>
-    </Router>
-  </QueryClientProvider>,
+  <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <UserContextProvider>
+          <App />
+        </UserContextProvider>
+      </Router>
+    </QueryClientProvider>
+  </ThemeProvider>
 )
